9b8ba5a64eabd260c1dab9cd4d918e8dc3908105
Stefan Falkenstein <sjfalken@gmail.com>
2022-03-16 14:24:32 -0400

    static fractal

--- src/mesh.rs
+++ src/mesh.rs
@@ -40,6 +40,95 @@ impl Vertex for MeshVertex {
     }
 }
 
+struct Fractal {
+    // counter-clockwise triangle vertices
+    base: [cgmath::Vector3<f32>; 3],
+    vertices: Vec<cgmath::Vector3<f32>>,
+    radius_scale: f32,
+}
+
+impl Fractal {
+    fn new(num_layers: u32) -> Self {
+        let q120 = cgmath::Quaternion::from_angle_z(cgmath::Deg(120.0f32));
+
+        let r = 0.8;
+        let top = cgmath::vec3(0., r, 0.);
+        let left = q120.rotate_vector(top);
+        let right = q120.rotate_vector(left);
+
+        let x0 = 0.0;
+        let y0 = 0.0;
+
+        let x1 = top[0];
+        let y1 = top[1];
+
+        let x2 = right[0];
+        let y2 = right[1];
+
+        let dist_to_line = ((x2 - x1)*(y1 - y0) - (x1 - x0)*(y2 - y1)).abs() / ((x2 - x1).powf(2.) + (y2 - y1).powf(2.)).sqrt();
+        let scale = dist_to_line / r; 
+
+        // let base_to_center_q = cgmath::Quaternion::from_angle_z(cgmath::Deg(180.0f32));
+        let mut result = Self {
+            base: [
+                top,
+                left,
+                right,
+            ],
+            vertices: Vec::new(),
+            radius_scale: scale,
+        };
+
+        result.add_black_centers(cgmath::vec3(0., 0., 0.), dist_to_line, num_layers);
+
+        result
+    }
+
+    fn add_black_centers(&mut self, origin: cgmath::Vector3<f32>, radius: f32, depth: u32) {
+        let q120 = cgmath::Quaternion::from_angle_z(cgmath::Deg(120.0f32));
+        let q60 = cgmath::Quaternion::from_angle_z(cgmath::Deg(60.0f32));
+
+        if depth == 0 {
+            return 
+        }
+
+        let bottom = cgmath::vec3(0., -radius, 0.);
+        let right = q120.rotate_vector(bottom);
+        let left = q120.rotate_vector(right);
+
+        self.vertices.push(bottom + origin);
+        self.vertices.push(right + origin);
+        self.vertices.push(left + origin);
+
+        let child_origin = q60.rotate_vector(bottom);
+        self.add_black_centers(child_origin + origin, self.radius_scale * radius, depth - 1);
+        let child_origin = q60.rotate_vector(right);
+        self.add_black_centers(child_origin + origin, self.radius_scale * radius, depth - 1);
+        let child_origin = q60.rotate_vector(left);
+        self.add_black_centers(child_origin + origin, self.radius_scale * radius, depth - 1);
+    }
+
+    fn into_mesh_vertices(self) -> Vec<MeshVertex> {
+        let mut result = Vec::new();
+        
+        for v in self.base {
+            result.push(MeshVertex {
+                color: [1., 1., 1., 1.],
+                position: v.into()
+            });
+        }
+
+        for v in self.vertices {
+            result.push(MeshVertex {
+                color: [0., 0., 0., 1.],
+                position: v.into(),
+            });
+        }
+
+        result
+    }
+}
+
 pub struct Mesh {
     pub vertex_buffer: wgpu::Buffer,
     pub index_buffer: wgpu::Buffer,
@@ -48,62 +137,10 @@ pub struct Mesh {
 
 impl Mesh {
     pub fn new(device: &wgpu::Device) -> Self {
-        let white = [1., 1., 1., 1.];
-        let black = [0., 0., 1., 1.];
 
-        // let white_long_len = 1.0;
-        // let white_short_len = (white_long_len.powf(2.0) / 2.0).sqrt();
+        let fractal = Fractal::new(8);
+        let mesh_vertices = fractal.into_mesh_vertices();
 
-        let q = cgmath::Quaternion::from_angle_z(cgmath::Deg(120.0f32));
-
-        let r = 0.8;
-        let tip = [0., r, 0.];
-        let left = q.rotate_vector(cgmath::Vector3::from(tip)).into();
-        let right = q.rotate_vector(cgmath::Vector3::from(left)).into();
-
-        let mut mesh_vertices = vec![
-            // white base triangle
-            MeshVertex {
-                position: tip,
-                color: white.clone(),
-            },
-            MeshVertex {
-                position: left,
-                color: white.clone(),
-            },
-            MeshVertex {
-                position: right,
-                color: white.clone(),
-            },
-        ];
-
-        let x0 = 0.0;
-        let y0 = 0.0;
-
-        let x1 = tip[0];
-        let y1 = tip[1];
-
-        let x2 = right[0];
-        let y2 = right[1];
-
-        let dist_to_line = ((x2 - x1)*(y1 - y0) - (x1 - x0)*(y2 - y1)).abs() / ((x2 - x1).powf(2.) + (y2 - y1).powf(2.)).sqrt();
-        let scale = dist_to_line / r; 
-
-        let q = cgmath::Quaternion::from_angle_z(cgmath::Deg(180.0f32));
-
-        for i in 0..mesh_vertices.len() {
-            mesh_vertices.push(MeshVertex {
-                position: (q.rotate_vector(cgmath::Vector3::from(mesh_vertices[i].position)) * scale).into(),
-                color: black.clone(),
-            });
-        }
-
-        // let num_layers = 1;
-        // for i in 0..num_layers {
-        //     for t in 0..(3u32.pow(i)) {
-                
-        //     }
-        // }
         let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: None,
             contents: bytemuck::cast_slice(&mesh_vertices),
